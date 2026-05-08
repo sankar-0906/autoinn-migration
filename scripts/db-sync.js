a41a4532040f70920eb7c0b2051b72dea62e7633
sankar <sankar.r@codingmart.com>
2026-05-08 17:42:04 +0530

    db change file

--- scripts/db-sync.js
+++ scripts/db-sync.js
@@ -22,7 +22,12 @@ async function syncDatabase() {
 
   const columnsToAdd = [
     { target: 'PurchaseSpareInvoice', column: 'createdBy', type: 'VARCHAR(25)' },
-    { target: 'PurchaseSpareInvoice', column: 'branch', type: 'VARCHAR(25)' }
+    { target: 'PurchaseSpareInvoice', column: 'branch', type: 'VARCHAR(25)' },
+    { target: 'VehicleInventory', column: 'createdBy', type: 'VARCHAR(25)' },
+    { target: 'VehicleInventory', column: 'branch', type: 'VARCHAR(25)' },
+    { target: 'VehicleInventory', column: 'color', type: 'VARCHAR(25)' },
+    { target: 'VehicleInventory', column: 'vehicle', type: 'VARCHAR(25)' },
+    { target: 'VehicleInventory', column: 'vehiclePurchase', type: 'VARCHAR(25)' }
   ];
 
   const joinTableRelations = [
